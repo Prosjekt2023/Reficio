ea19de6d3eeb188d4b4f34e76f6ad55b9e936254
johannesUIA <johannesma@student.uia.no>
2023-11-07 19:17:13 +0000

    Prøve å opprette sjekkliste overview
    Opprett controller for:
    -Sjekklister
    -FilledOutCheckList
    Opprettet Index:
    -Sjekklister
    -FilledOutchCheckList

---
+++ bacit-dotnet.MVC/Controllers/FilledOutCheckListController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using bacit_dotnet.MVC.Repositories;
+
+namespace bacit_dotnet.MVC.Controllers;
+
+public class FilledOutCheckListController : Controller
+{
+    private readonly CheckListRepository _repository;
+
+    public FilledOutCheckListController(CheckListRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    /*public IActionResult Index()
+    {
+        var CheckList = _repository.GetAll();
+        return View(CheckList);
+    }*/
+    
+    public IActionResult Index(int id)
+    {
+        var CheckList = _repository.GetOneRowById(id);
+        if (CheckList == null)
+        {
+            return NotFound();
+        }
+        return View(CheckList);
+    }
+}
