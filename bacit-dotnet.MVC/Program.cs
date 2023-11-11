80691bd7281e2f46c4e3d9e783b0263eb9e798d1
johannestj <johannestj@student.uia.no>
2023-11-11 15:57:39 +0100

    Fisket program.cs slik at docker containeren holder seg kjørende etter den er bygd

--- bacit-dotnet.MVC/Program.cs
+++ bacit-dotnet.MVC/Program.cs
@@ -15,126 +15,129 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
-public class Program
+namespace bacit_dotnet.MVC
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.WebHost.ConfigureKestrel(x => x.AddServerHeader = false);
-
-        // Add services to the container.
-        builder.Services.AddControllersWithViews(options =>
+        public static void Main(string[] args)
         {
-            options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-        });
+            var builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.ConfigureKestrel(x => x.AddServerHeader = false);
 
-        // Configure the database connection.
-        builder.Services.AddScoped<IDbConnection>(_ =>
-        {
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            return new MySqlConnection(connectionString);
-        });
+            // Add services to the container.
+            builder.Services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            });
 
-        // Register your repository here.
-        builder.Services.AddTransient<ServiceFormRepository>();
+            // Configure the database connection.
+            builder.Services.AddScoped<IDbConnection>(_ =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+                return new MySqlConnection(connectionString);
+            });
 
-        builder.Services.AddTransient<CheckListRepository>();
+            // Register your repository here.
+            builder.Services.AddTransient<ServiceFormRepository>();
 
-        SetupDataConnections(builder);
+            builder.Services.AddTransient<CheckListRepository>();
 
-        builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
-        builder.Services.AddSingleton<IUserRepository, SqlUserRepository>();
-        builder.Services.AddSingleton<IUserRepository, DapperUserRepository>();
+            SetupDataConnections(builder);
 
-        SetupAuthentication(builder);
+            builder.Services.AddTransient<IUserRepository, InMemoryUserRepository>();
+            builder.Services.AddTransient<IUserRepository, SqlUserRepository>();
+            builder.Services.AddTransient<IUserRepository, DapperUserRepository>();
 
-        var app = builder.Build();
+            SetupAuthentication(builder);
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            UseAuthentication(app);
+
+            app.MapControllerRoute(name: "default", pattern: "{controller=Account}/{action=Login}/{id?}");
+            app.MapControllers();
+
+            app.Run();
+
+            builder.Services.AddAntiforgery(options => { options.HeaderName = "X-CSRF-TOKEN"; });
+
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel(c => c.AddServerHeader = false)
+                .UseStartup<Startup>()
+                .Build();
         }
 
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        UseAuthentication(app);
-
-        app.MapControllerRoute(name: "default", pattern: "{controller=Account}/{action=Login}/{id?}");
-        app.MapControllers();
-
-        app.Run();
-
-        builder.Services.AddAntiforgery(options => { options.HeaderName = "X-CSRF-TOKEN"; });
-
-        WebHost.CreateDefaultBuilder(args)
-            .ConfigureKestrel(c => c.AddServerHeader = false)
-            .UseStartup<Startup>()
-            .Build();
-    }
-
-    private static void SetupDataConnections(WebApplicationBuilder builder)
-    {
-        builder.Services.AddTransient<ISqlConnector, SqlConnector>();
-
-        builder.Services.AddDbContext<DataContext>(options =>
+        private static void SetupDataConnections(WebApplicationBuilder builder)
         {
-            options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-                ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")));
-        });
+            builder.Services.AddTransient<ISqlConnector, SqlConnector>();
 
-    }
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+                    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")));
+            });
 
-    private static void UseAuthentication(WebApplication app)
-    {
-        app.UseAuthentication();
-        app.UseAuthorization();
-    }
+        }
 
-    private static void SetupAuthentication(WebApplicationBuilder builder)
-    {
-        //Setup for Authentication
-        builder.Services.Configure<IdentityOptions>(options =>
+        private static void UseAuthentication(WebApplication app)
         {
-            // Default Lockout settings.
-            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            options.Lockout.MaxFailedAccessAttempts = 5;
-            options.Lockout.AllowedForNewUsers = false;
-            options.SignIn.RequireConfirmedPhoneNumber = false;
-            options.SignIn.RequireConfirmedEmail = false;
-            options.SignIn.RequireConfirmedAccount = false;
-            options.User.RequireUniqueEmail = true;
-        });
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
 
-        builder.Services
-            .AddIdentityCore<IdentityUser>()
-            .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<DataContext>()
-            .AddSignInManager()
-            .AddDefaultTokenProviders();
-
-        builder.Services.AddAuthentication(o =>
+        private static void SetupAuthentication(WebApplicationBuilder builder)
         {
-            o.DefaultScheme = IdentityConstants.ApplicationScheme;
-            o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            //Setup for Authentication
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Default Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+            });
 
-        }).AddIdentityCookies(o => { });
+            builder.Services
+                .AddIdentityCore<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<DataContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
 
-        builder.Services.AddTransient<IEmailSender, AuthMessageSender>();
-    }
+            builder.Services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = IdentityConstants.ApplicationScheme;
+                o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
 
-    public class AuthMessageSender : IEmailSender
-    {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+            }).AddIdentityCookies(o => { });
+
+            builder.Services.AddTransient<IEmailSender, AuthMessageSender>();
+        }
+
+        public class AuthMessageSender : IEmailSender
         {
-            Console.WriteLine(email);
-            Console.WriteLine(subject);
-            Console.WriteLine(htmlMessage);
-            return Task.CompletedTask;
+            public Task SendEmailAsync(string email, string subject, string htmlMessage)
+            {
+                Console.WriteLine(email);
+                Console.WriteLine(subject);
+                Console.WriteLine(htmlMessage);
+                return Task.CompletedTask;
+            }
         }
     }
 }
