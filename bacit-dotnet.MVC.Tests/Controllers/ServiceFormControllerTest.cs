1fc28d7156d1968e48327c21c6a47b6098fcc9e4
Jeppess123 <112609426+Jeppess123@users.noreply.github.com>
2023-11-16 15:31:49 +0100

    lager testing filer

---
+++ bacit-dotnet.MVC.Tests/Controllers/ServiceFormControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using bacit_dotnet.MVC.Controllers;
+using bacit_dotnet.MVC.Models.ServiceForm;
+using bacit_dotnet.MVC.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace YourNamespace.Tests
+{
+    public class ServiceFormControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ServiceFormRepository>();
+            var controller = new ServiceFormController(repositoryMock.Object);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void IndexPost_ValidModel_RedirectsToServiceOrderIndex()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ServiceFormRepository>();
+            var controller = new ServiceFormController(repositoryMock.Object);
+            var validModel = new ServiceFormViewModel
+            {
+                // Sett gyldige egenskaper for modellen her
+            };
+
+            // Act
+            var result = controller.Index(validModel) as RedirectToActionResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Index", result.ActionName);
+            Assert.Equal("ServiceOrder", result.ControllerName);
+        }
+
+        [Fact]
+        public void IndexPost_InvalidModel_ReturnsViewResult()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ServiceFormRepository>();
+            var controller = new ServiceFormController(repositoryMock.Object);
+            var invalidModel = new ServiceFormViewModel
+            {
+                // Sett ugyldige egenskaper for modellen her
+            };
+            controller.ModelState.AddModelError("Egenskapsnavn", "Feilmelding");
+
+            // Act
+            var result = controller.Index(invalidModel);
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(invalidModel, ((ViewResult)result).Model);
+        }
+    }
+}
