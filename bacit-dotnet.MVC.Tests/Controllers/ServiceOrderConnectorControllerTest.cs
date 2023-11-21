5a51955d614e3c45ee85c1e75ae246cb27412b21
johannesUIA <johannesma@student.uia.no>
2023-11-21 10:34:07 +0100

    Revert "Merge remote-tracking branch 'origin/Unit_Tests' into pre-main"
    This reverts commit 539928f713af531d513a7578f9fb98809ca50621, reversing
    changes made to f695cbcdf7a2e143125868ad277ea14eb5a04022.

--- bacit-dotnet.MVC.Tests/Controllers/ServiceOrderConnectorControllerTest.cs
+++ bacit-dotnet.MVC.Tests/Controllers/ServiceOrderConnectorControllerTest.cs
@@ -1,55 +1,6 @@
-
 namespace bacit_dotnet.MVC.Tests.Controllers;
 
 public class ServiceOrderConnectorControllerTest
 {
     
-using bacit_dotnet.MVC.Controllers;
-using bacit_dotnet.MVC.Models.Composite;
-using bacit_dotnet.MVC.Repositories;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
-using Xunit;
-
-public class ServiceOrderConnectorControllerTests
-{
-    [Fact]
-    public void Index_ReturnsViewWithModel_WhenValidId()
-    {
-        // Arrange
-        var mockServiceFormRepository = new Mock<IServiceFormRepository>();
-        var mockCheckListRepository = new Mock<ICheckListRepository>();
-        var controller = new ServiceOrderConnectorController(mockServiceFormRepository.Object, mockCheckListRepository.Object);
-
-        var serviceFormEntry = new ServiceFormEntry(); // Erstatt med reell klasse og data
-        var checkListEntry = new CheckListEntry(); // Erstatt med reell klasse og data
-
-        mockServiceFormRepository.Setup(repo => repo.GetRelevantData(It.IsAny<int>())).Returns(serviceFormEntry);
-        mockCheckListRepository.Setup(repo => repo.GetRelevantData(It.IsAny<int>())).Returns(checkListEntry);
-
-        // Act
-        var result = controller.Index(1) as ViewResult;
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(serviceFormEntry, (result.Model as CompositeViewModel)?.ServiceForm);
-        Assert.Equal(checkListEntry, (result.Model as CompositeViewModel)?.CheckList);
-    }
-
-    [Fact]
-    public void Index_ReturnsNotFound_WhenInvalidId()
-    {
-        // Arrange
-        var mockServiceFormRepository = new Mock<IServiceFormRepository>();
-        var mockCheckListRepository = new Mock<ICheckListRepository>();
-        var controller = new ServiceOrderConnectorController(mockServiceFormRepository.Object, mockCheckListRepository.Object);
-
-        mockServiceFormRepository.Setup(repo => repo.GetRelevantData(It.IsAny<int>())).Returns((ServiceFormEntry)null);
-
-        // Act
-        var result = controller.Index(2) as NotFoundResult;
-
-        // Assert
-        Assert.NotNull(result);
-    }
 }
--- bacit-dotnet.MVC.Tests/Controllers/ServiceOrderControllerTest.cs
+++ bacit-dotnet.MVC.Tests/Controllers/ServiceOrderControllerTest.cs
@@ -1,49 +1,6 @@
-
 namespace bacit_dotnet.MVC.Tests.Controllers;
 
 public class ServiceOrderConnectorControllerTest
 {
     
-using bacit_dotnet.MVC.Controllers;
-using bacit_dotnet.MVC.Repositories;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
-using Xunit;
-
-public class ServiceOrderControllerTests
-{
-    [Fact]
-    public void Index_ReturnsViewWithModel()
-    {
-        // Arrange
-        var mockRepository = new Mock<IServiceFormRepository>();
-        var controller = new ServiceOrderController(mockRepository.Object);
-        var serviceFormEntry = new ServiceFormEntry(); // Erstatt med reell klasse og data
-
-        mockRepository.Setup(repo => repo.GetSomeOrderInfo()).Returns(serviceFormEntry);
-
-        // Act
-        var result = controller.Index() as ViewResult;
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(serviceFormEntry, result.Model);
-    }
-
-    [Fact]
-    public void Index_ReturnsUnauthorized_WhenNotAuthenticated()
-    {
-        // Arrange
-        var controller = new ServiceOrderController(Mock.Of<IServiceFormRepository>());
-        controller.ControllerContext = new ControllerContext();
-        // Simulerer ikke-autentisert tilstand
-        controller.ControllerContext.HttpContext = new DefaultHttpContext { User = new ClaimsPrincipal() };
-
-        // Act
-        var result = controller.Index() as UnauthorizedResult;
-
-        // Assert
-        Assert.NotNull(result);
-    }
 }
