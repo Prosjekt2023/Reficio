efc2321785f09f2994ddfd3490d6b81352fbd22c
johannesUIA <johannesma@student.uia.no>
2023-11-24 12:34:03 +0100

    Fjerner Chekclist oversikt
    -Fjernet GetSomeOrderInfo fra Checklist repository og irepsoitory
    -Fjerne ChecklistOrder controller og view
    -Kommentert kode
    -Fjerne GetAll fra checklist repositroy

--- bacit-dotnet.MVC/Controllers/CheckListController.cs
+++ bacit-dotnet.MVC/Controllers/CheckListController.cs
@@ -1,7 +1,7 @@
 /*
- *Forfatter Johannes aka Lord Of CheckLists
- * Patch 2.1 Redirigerer etter opprettelse av instanse til spesifike int verdi
- * Irepository er også oppdatert fra void tul int
+ *Author Johannes aka Lord Of CheckLists
+ * Patch 2.1 patch makes it possible to view created checklists in realtime
+ * Irepository is updated from void to int
  */
 
 using bacit_dotnet.MVC.Models.Composite;
@@ -10,6 +10,9 @@ using bacit_dotnet.MVC.Repositories;
 
 namespace bacit_dotnet.MVC.Controllers
 {
+    /*
+     *The controller's constructor accepts an ICheckListRepository object.
+     *The _repository private readonly field stores the repository object.*/
     public class CheckListController : Controller
     {
         private readonly ICheckListRepository _repository;
@@ -18,20 +21,28 @@ namespace bacit_dotnet.MVC.Controllers
         {
             _repository = repository;
         }
-
+       /*
+        * public IActionResult Index() is a method that handles HTTP GET requests
+        * It returns the default view associated with this action,
+        * which is typically a page where users can view or start filling out a checklist
+        */
         public IActionResult Index()
         {
             return View();
         }
-
+        /*
+         * [HttpPost]from the Index method deals with POST requests
+         * -from CheckList viewpage that uses the POST Method to submit the form data
+         * 
+         */
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Index(CheckListViewModel checkListViewModel)
         {
             if (ModelState.IsValid)
             {
-                var id = _repository.Insert(checkListViewModel); // Når instansen insertes i databsen returneres int verdien
-                return RedirectToAction("Index", "FilledOutCheckList", new { id = id }); // Redirigerer til FilledOutCheckListController
+                var id = _repository.Insert(checkListViewModel); // Returns the entry that was created using the defined method in the repository
+                return RedirectToAction("Index", "FilledOutCheckList", new { id = id }); // Reddirects to FilledOutCheckListController
             }
             
             return View(checkListViewModel);
