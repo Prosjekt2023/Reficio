a3f9f6e67819ea01a64e129cab236daa0509b410
jonhovd <jonhovd@outlook.com>
2023-11-25 12:43:04 +0100

    Kommentert mer

--- bacit-dotnet.MVC/Program.cs
+++ bacit-dotnet.MVC/Program.cs
@@ -18,6 +18,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace bacit_dotnet.MVC
 {
+    /// <summary>
+    /// The Program class is the main entry point for an ASP.NET Core web application.
+    /// </summary>
     public class Program
     {
         /// <summary>
@@ -26,25 +29,18 @@ namespace bacit_dotnet.MVC
         /// <param name="args">Command line arguments passed to the program.</param>
         public static void Main(string[] args)
         {
-            // Creates a new WebApplication builder with the arguments passed to the program.
-            // This typically includes configurations and services that the application will use.
+            // Initialize the WebApplication builder
             var builder = WebApplication.CreateBuilder(args);
-            // Configures the Kestrel server, which is the web server included with ASP.NET Core.
-            // Specifically, this configuration sets not to include the server header in the response,
-            // which is a common security practice to minimize information about the server infrastructure.
+            // Configure the Kestrel web server
             builder.WebHost.ConfigureKestrel(x => x.AddServerHeader = false);
             
-            // Adds MVC controllers with their associated views to the application's services.
-            // This includes a configuration to add a filter that automatically validates anti-forgery tokens for all incoming requests
-            // to prevent cross-site request forgery (CSRF) attacks. This filter ensures that your application validates tokens on POST, PUT, DELETE, and PATCH requests.
+            // Add MVC controllers with views and configure anti-forgery measures
             builder.Services.AddControllersWithViews(options =>
             {
                 options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
             });
 
-            // Configure the database connection.
-            // Retrieves the connection string named "DefaultConnection" from the application's configuration,
-            // which is typically defined in the appsettings.json file or through environment variables.
+            // Configure database connection
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddScoped<IDbConnection>(_ => new MySqlConnection(connectionString));
             
@@ -55,9 +51,6 @@ namespace bacit_dotnet.MVC
             });
 
             // Register your repository here.
-            
-            // Adds a transient service of type IServiceFormRepository to the dependency injection container,
-            // with the concrete implementation being ServiceFormRepository. Transient services are created each time they are requested
             builder.Services.AddTransient<IServiceFormRepository, ServiceFormRepository>();
             builder.Services.AddTransient<ICheckListRepository, CheckListRepository>();
             builder.Services.AddTransient<IUserRepository, InMemoryUserRepository>();
@@ -66,36 +59,25 @@ namespace bacit_dotnet.MVC
             builder.Services.AddScoped<IUserRepository, EFUserRepository>();
                 
             
-            // Calls a method to set up the data connections for the application.
-            // This would typically involve configuring the database contexts, connection strings,
+            // Setup data connections and authentication
             SetupDataConnections(builder);
-            // Calls a method to set up the authentication services for the application.
-            // This includes setting up the identity management, authentication schemes, 
-            // cookie settings, and any other security-related services or configurations
             SetupAuthentication(builder);
             
             // Adds and configures the anti-forgery service which is used to generate anti-forgery tokens.
-            // These tokens are used to prevent Cross-Site Request Forgery (CSRF) attacks.
-            // Here, the header name for the token in HTTP requests is set to "X-CSRF-TOKEN".
             builder.Services.AddAntiforgery(options => {
                 options.HeaderName = "X-CSRF-TOKEN";
             });
 
-            // Builds the web application using the configurations and services defined in 'builder'.
-            // This includes compiling the middleware pipeline and making the application ready to start accepting requests.
+            // Build the application
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            // Checks if the application is not running in the Development environment.
             if (!app.Environment.IsDevelopment())
             {
                 // Configures the app to use a custom error handler page located at '/Home/Error'.
                 // This is where the application will redirect for any unhandled exceptions in non-Development environments.
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // Enforces the use of HTTPS Strict Transport Security (HSTS) by adding the HSTS header to responses.
-                // This header tells browsers to only access the application over HTTPS for a period of time (default is 30 days).
-                // HSTS is a security feature to force clients to use HTTPS, thus reducing the risk of man-in-the-middle attacks.
                 app.UseHsts();
             }
             
@@ -140,16 +122,31 @@ namespace bacit_dotnet.MVC
 
             UseAuthentication(app);
 
+            // Map controller routes
             app.MapControllerRoute(name: "default", pattern: "{controller=Account}/{action=Login}/{id?}");
             app.MapControllers();
             // Runs the application. This is the call that starts listening for incoming HTTP requests.
             app.Run();
         }
 
+        
+        /// <summary>
+        /// Configures and sets up the data connections for the web application.
+        /// </summary>
+        /// <remarks>
+        /// This method is responsible for registering services related to database connectivity and operations.
+        /// It adds the necessary DbContext and configures it for use with a MySQL database.
+        /// </remarks>
+        /// <param name="builder">The WebApplicationBuilder instance used to configure and build the application.</param>
         private static void SetupDataConnections(WebApplicationBuilder builder)
         {
+            // Registers the ISqlConnector interface with its concrete implementation SqlConnector.
+            // The service is registered with a transient lifetime, meaning a new instance will be created each time it is injected
             builder.Services.AddTransient<ISqlConnector, SqlConnector>();
-
+            
+            // Adds and configures the DataContext for Entity Framework Core.
+            // Configures the DataContext to use MySQL with the connection string from the application configuration.
+            // The MySQL server version is auto-detected for compatibility.
             builder.Services.AddDbContext<DataContext>(options =>
             {
                 options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -158,46 +155,95 @@ namespace bacit_dotnet.MVC
 
         }
 
+        
+        /// <summary>
+        /// Configures the authentication and authorization middleware for the web application.
+        /// </summary>
+        /// <remarks>
+        /// This method is critical for enforcing security in the application. It enables the middleware 
+        /// that processes authentication and authorization headers in incoming HTTP requests. 
+        /// Authentication middleware identifies the user making the request, while authorization middleware 
+        /// checks if the authenticated user has the necessary permissions to access the requested resources.
+        /// </remarks>
+        /// <param name="app">The WebApplication instance representing the running application.</param>
         private static void UseAuthentication(WebApplication app)
         {
+            // Enables authentication middleware, which processes authentication data in the request headers 
+            // to identify the user on each request.
             app.UseAuthentication();
             app.UseAuthorization();
         }
-
+        
+        /// <summary>
+        /// Configures authentication services and settings for the web application.
+        /// </summary>
+        /// <remarks>
+        /// This method sets up the necessary components for handling user authentication in the application.
+        /// It configures the identity options, adds the identity services, and specifies the authentication 
+        /// schemes. The method is integral for managing user accounts, roles, and how users sign in.
+        /// </remarks>
+        /// <param name="builder">The WebApplicationBuilder used to configure services for the application.</param>
         private static void SetupAuthentication(WebApplicationBuilder builder)
         {
-            //Setup for Authentication
+            // Configures the IdentityOptions for the application.
+            // This includes lockout settings, sign-in requirements, and user options.
             builder.Services.Configure<IdentityOptions>(options =>
             {
-                // Default Lockout settings.
+                // Sets the default lockout duration to 5 minutes and maximum failed access attempts to 5.
+                // Disables lockout for new users.
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.AllowedForNewUsers = false;
+                // Disables requirements for confirmed phone number, email, and confirmed accounts for signing in.
+                // Requires a unique email for each user.
                 options.SignIn.RequireConfirmedPhoneNumber = false;
                 options.SignIn.RequireConfirmedEmail = false;
                 options.SignIn.RequireConfirmedAccount = false;
                 options.User.RequireUniqueEmail = true;
             });
-
+            // Adds and configures the Identity services.
+            // This includes setting up roles, entity framework stores for data persistence,
+            // sign-in management, and token providers.
             builder.Services
                 .AddIdentityCore<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<DataContext>()
                 .AddSignInManager()
                 .AddDefaultTokenProviders();
-
+            
+            // Configures the authentication schemes for the application.
             builder.Services.AddAuthentication(o =>
             {
                 o.DefaultScheme = IdentityConstants.ApplicationScheme;
                 o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
 
             }).AddIdentityCookies(o => { });
-
+            
+            // Registers a transient service for sending emails, using the AuthMessageSender implementation.
             builder.Services.AddTransient<IEmailSender, AuthMessageSender>();
         }
-
+        
+        /// <summary>
+        /// A class that implements the IEmailSender interface for sending emails.
+        /// </summary>
+        /// <remarks>
+        /// This implementation is a basic example and only writes the email details to the console.
+        /// In a production scenario, this method should be replaced with actual email sending logic,
+        /// potentially using an email service provider.
+        /// </remarks>
         public class AuthMessageSender : IEmailSender
         {
+            /// <summary>
+            /// Asynchronously sends an email.
+            /// </summary>
+            /// <param name="email">The destination email address.</param>
+            /// <param name="subject">The subject of the email.</param>
+            /// <param name="htmlMessage">The HTML message body of the email.</param>
+            /// <returns>A task that represents the asynchronous operation.</returns>
+            /// <remarks>
+            /// This implementation is for demonstration purposes and does not actually send an email.
+            /// It writes the email details to the console. Replace this with actual email sending logic.
+            /// </remarks>
             public Task SendEmailAsync(string email, string subject, string htmlMessage)
             {
                 Console.WriteLine(email);
