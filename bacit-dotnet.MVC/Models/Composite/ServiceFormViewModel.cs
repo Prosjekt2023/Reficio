3b87994217dc38ff4f9a9e6fb52acff435a656f3
TonyLe02 <tonynl@student.uia.no>
2023-11-26 15:01:56 +0100

    Update ServiceFormViewModel.cs
    Fjernet unødvendigheter

--- bacit-dotnet.MVC/Models/Composite/ServiceFormViewModel.cs
+++ bacit-dotnet.MVC/Models/Composite/ServiceFormViewModel.cs
@@ -6,10 +6,8 @@ public class ServiceFormViewModel
 {
     public int ServiceFormId { get; set; }
 
-    [Required(ErrorMessage = "Customer is required.")]
     public string Customer { get; set; }
 
-    [Required(ErrorMessage = "DateReceived is required.")]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
     public DateTime DateReceived { get; set; }
 
@@ -29,7 +27,6 @@ public class ServiceFormViewModel
     public string UsedParts { get; set; }
     public string WorkHours { get; set; }
 
-    [Required(ErrorMessage = "CompletionDate is required.")]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
     public DateTime CompletionDate { get; set; }
 
