59f99bf431a10f4d8aeefc48b7f7da7309c1b297
TonyLe02 <tonynl@student.uia.no>
2023-11-26 12:44:09 +0100

    push

--- bacit-dotnet.MVC/Repositories/IServiceFormRepository.cs
+++ bacit-dotnet.MVC/Repositories/IServiceFormRepository.cs
@@ -1,18 +1,13 @@
-
 using bacit_dotnet.MVC.Models.Composite;
-
 
 namespace bacit_dotnet.MVC.Repositories;
 
-    public interface IServiceFormRepository
-    {
-        public IEnumerable<ServiceFormViewModel> GetAll();
+public interface IServiceFormRepository
+{
+    public IEnumerable<ServiceFormViewModel> GetSomeOrderInfo();
+    public ServiceFormViewModel GetRelevantData(int id);
 
-        public IEnumerable<ServiceFormViewModel> GetSomeOrderInfo();
-        public ServiceFormViewModel GetRelevantData(int id);
+    public ServiceFormViewModel GetOneRowById(int id);
 
-        public ServiceFormViewModel GetOneRowById(int id);
-        
-        void Insert(ServiceFormViewModel serviceFormViewModel);
-        
-    } 
+    void Insert(ServiceFormViewModel serviceFormViewModel);
+}
